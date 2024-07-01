571810d7582e399876d3f455f84410b0e57d92d7
PadBro <padmaster2012@googlemail.com>
2024-07-01 15:45:27 +0200

    fix linter

--- pages/index.vue
+++ pages/index.vue
@@ -37,7 +37,9 @@
                   class="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
                   >Join our Discord</NuxtLink
                 >
-                <NuxtLink href="/about" class="text-sm font-semibold leading-6 text-white"
+                <NuxtLink
+                  href="/about"
+                  class="text-sm font-semibold leading-6 text-white"
                   >Learn more <span aria-hidden="true">→</span></NuxtLink
                 >
               </div>
